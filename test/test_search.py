16c2be9d493934cad4c622f8cbb1f658bc67a9c9
abdush <abduhussein@gmail.com>
2014-03-27 20:36:16 +0000

    search author page

--- test/test_search.py
+++ test/test_search.py
@@ -28,19 +28,19 @@ class TestSearch(unittest.TestCase):
             "incorrect number of coauthors")"""
 
     def test_search_first_author(self):
-        authorName = "Stefano Ceri"
+        authorName = "AUTHOR1"
         db = database.Database()
-        self.assertTrue(db.read(path.join(self.data_dir, "publications_small_sample.xml")))
+        self.assertTrue(db.read(path.join(self.data_dir, "sprint-2-acceptance-2.xml")))
         data = db.get_first_author_stats(authorName)
         self.assertEqual(data[0],authorName,
             "incorrect author")
-        self.assertEqual(data[1], 0,
+        self.assertEqual(data[1], 2,
             "incorrect number of times he appears first in conference paper")
         self.assertEqual(data[2], 0,
             "incorrect number of times he appears first in journal")
-        self.assertEqual(data[3], 1,
+        self.assertEqual(data[3], 0,
             "incorrect number of times he appears first in book")
-        self.assertEqual(data[4], 1,
+        self.assertEqual(data[4], 0,
             "incorrect number of times he appears first in book chapter")
         self.assertEqual(data[5], 2,
             "incorrect number of times he appears first in total")
@@ -62,3 +62,39 @@ class TestSearch(unittest.TestCase):
             "incorrect number of times he appears first in book chapter")
         self.assertEqual(data[5], 0,
             "incorrect number of times he appears first in total")
+
+    def test_search_sole_author_pub(self):
+        authorName = "AUTHOR1"
+        db = database.Database()
+        self.assertTrue(db.read(path.join(self.data_dir, "sprint-2-acceptance-2.xml")))
+        data = db.get_sole_author_stats(authorName)
+        self.assertEqual(data[0],authorName,
+            "incorrect author")
+        self.assertEqual(data[1], 1,
+            "incorrect number of conference papers")
+        self.assertEqual(data[2], 0,
+            "incorrect number of journals")
+        self.assertEqual(data[3], 0,
+            "incorrect number of books")
+        self.assertEqual(data[4], 0,
+            "incorrect number of book chapters")
+        self.assertEqual(data[5], 1,
+            "incorrect number of total")
+
+    def test_search_all_author_pub(self):
+        authorName = "AUTHOR1"
+        db = database.Database()
+        self.assertTrue(db.read(path.join(self.data_dir, "sprint-2-acceptance-2.xml")))
+        data = db.get_all_author_stats(authorName)
+        self.assertEqual(data[0],authorName,
+            "incorrect author")
+        self.assertEqual(data[1], 3,
+            "incorrect number of conference papers")
+        self.assertEqual(data[2], 0,
+            "incorrect number of journals")
+        self.assertEqual(data[3], 0,
+            "incorrect number of books")
+        self.assertEqual(data[4], 0,
+            "incorrect number of book chapters")
+        self.assertEqual(data[5], 3,
+            "incorrect number of total")
